b7effefbf4358f0ddf398bea874d5a2164b0bd4c
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2024-09-15 16:49:18 +0200

    Added oldest/newest video stat and stats saving to markdown file.

--- src/stats.py
+++ src/stats.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 
 
 def get_data_from_file(filename):
@@ -7,8 +8,7 @@ def get_data_from_file(filename):
 
 
 def total_videos(data):
-    total_videos = len(data)
-    print(f"Total videos {total_videos}")
+    return len(data)
 
 
 def videos_per_channel(data):
@@ -19,5 +19,40 @@ def videos_per_channel(data):
             counter[channel] = 0
         counter[channel] += 1
 
-    for key in counter:
-        print(f"{key}: {counter[key]}")
+    return counter
+
+
+def get_datetime(date):
+    return datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%fZ")
+
+
+def get_oldest(data):
+    date = get_datetime(data[len(data) - 1]["time"])
+    return date.strftime("%Y-%m-%d %H:%M:%S")
+
+
+def get_newest(data):
+    date = get_datetime(data[0]["time"])
+    return date.strftime("%Y-%m-%d %H:%M:%S")
+
+
+def save_stats(data):
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    frequency_per_channel = sorted(videos_per_channel(data).items(), key=lambda x:(-x[1], x[0]))
+
+    with open(f"statistics_{current_time}.md", "w", encoding="utf-8") as file:
+        file.write("# Watch history statistics\n")
+        file.write(f"Total videos: {total_videos(data)}\n\n")
+        file.write(f"Total channels: {len(frequency_per_channel)}\n\n")
+        file.write(f"Oldest video: {get_oldest(data)}\n\n")
+        file.write(f"Newest video: {get_newest(data)}\n\n")
+
+        file.write("## Videos per channel\n")
+        for count, channel in enumerate(frequency_per_channel, 1):
+            percentage = (channel[1] / total_videos(data))
+            file.write(f"{count}. {channel[0]}: {channel[1]} ({percentage:.2%})\n")
+
+
+def total_stats(filename):
+    data = get_data_from_file(filename)
+    save_stats(data)
