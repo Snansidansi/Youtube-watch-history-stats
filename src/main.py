822c70a1e8814c291750324e312796b8d9800742
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2024-09-16 11:50:33 +0200

    Added main file.

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import sys
+
+from src import stats
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("Usage: script filepath threshold")
+        print("script: filepath of the youtube watch history ad .json file")
+        print("threshold: threshold for the diagram. " +
+              "Minimum number of videos viewed from a channel to show it in the diagram")
+    elif len(sys.argv) == 3:
+        try:
+            stats.total_stats(sys.argv[1], int(sys.argv[2]))
+        except FileNotFoundError:
+            print(f"File not found: {sys.argv[1]}")
+    else:
+        print("Usage: script filepath threshold")
+        print("Or run without parameters for help.")
