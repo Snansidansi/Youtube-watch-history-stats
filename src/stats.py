6f0694b6829bac5214a205a4750c81d460bade6d
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2024-09-16 10:44:40 +0200

    Some fixes for the layout of the stats.

--- src/stats.py
+++ src/stats.py
@@ -50,46 +50,50 @@ def get_newest(data):
     return date.strftime("%Y-%m-%d %H:%M:%S")
 
 
-def save_stats(data):
+def save_stats(data, threshold):
     current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     frequency_per_channel = sort_videos_by_channel(videos_per_channel(data))
+    diagram_name = f"frequency-per-channel_{current_time}"
 
     with open(f"statistics_{current_time}.md", "w", encoding="utf-8") as file:
         file.write("# Watch history statistics\n")
         file.write(f"Total videos: {total_videos(data)}\n\n")
         file.write(f"Total channels: {len(frequency_per_channel)}\n\n")
-        file.write(f"Oldest video: {get_oldest(data)}\n\n")
         file.write(f"Newest video: {get_newest(data)}\n\n")
+        file.write(f"Oldest video: {get_oldest(data)}\n\n")
 
         file.write("## Videos per channel\n")
         for count, channel in enumerate(frequency_per_channel, 1):
             percentage = (channel[1] / total_videos(data))
             file.write(f"{count}. {channel[0]}: {channel[1]} ({percentage:.2%})\n")
 
+        file.write(f"\n![]({diagram_name}.png)")
 
-def create_diagram(data, threshold):
-    counter = dict(sort_videos_by_channel(threshold_videos_by_channel(videos_per_channel(data), threshold)))
+    create_diagram(dict(frequency_per_channel), threshold, diagram_name, data)
+
+
+def create_diagram(frequency, threshold, name, data):
+    counter = dict(threshold_videos_by_channel(frequency, threshold))
     x = list(counter.keys())
     y = list(counter.values())
     label_length = max([len(channel) for channel in counter])
 
     fig_size = plt.gcf().get_size_inches()
     plt.figure(figsize=(fig_size[0], fig_size[1] + label_length * 0.05))
-
     plt.style.use("fast")
 
-    plt.title(f"Videos per channel (threshold: {threshold})")
+    plt.title(f"Videos per channel (threshold: {threshold} --- {sum(y)}/{total_videos(data)})")
     plt.xlabel("Channel")
     plt.ylabel("Videos")
 
     plt.xticks(rotation=90)
     plt.yticks(y)
-
     plt.bar(x, y, width=0.75)
     plt.tight_layout()
-    plt.show()
+
+    plt.savefig(name)
 
 
 def total_stats(filename):
     data = get_data_from_file(filename)
-    save_stats(data)
+    save_stats(data, 0)
